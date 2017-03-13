0020dbbc8ed38862b85cd0c5730401360cbc5a8e
James Sharp <jamesrosssharp@gmail.com>
2017-03-13 18:12:05 +1100

    Finished off real mode wrapper... now successfully calling real mode from protected mode

--- OSlet/OS/Boot/boot.asm
+++ OSlet/OS/Boot/boot.asm
@@ -11,22 +11,25 @@
 KERNEL_SEG	equ	02000h
 KERNEL_OFFSET	equ	KERNEL_SEG*10h
 
-
-
 ;======================== Entry from Bootloader	==============================
 
+kernel_entry_point:
 start:
+
+	mov eax, 00000010h
+	mov ds, ax
 
 	; We don't trust the bootloader's idt and gdt.
 	; Load our own here.
 
-
-
+	lidt [kernel_idt_ptr]
+	lgdt [kernel_gdt_ptr]
 
 	; Long jump to set cs segment
-	
 
+	jmp	08h:set_cs	
 
+set_cs:
 
 	; rest of init
 
@@ -38,10 +41,51 @@ start:
 	mov ss, ax
 	mov esp, 0008fff0h	; Move the stack well away from the EBDA typical location
 
-	finit
+	finit			; Should really do this once we have detected
+				; that the CPU has an FPU
 
 	jmp	main
 
+;======================		Kernel gdt and idt
+
+kernel_gdt:
+	dw 0,0,0,0	; null descriptor
+
+	; code selector
+	dw 00ffffh	; 4GB limit
+	dw 00000h
+	dw 9a00h	; executable, read only
+	dw 00cfh
+
+	; data selector
+	dw 0ffffh
+	dw 00000h
+	dw 9200h
+	dw 00cfh
+
+	; code selector for transition into real mode
+	dw 0ffffh
+	dw 00000h
+	dw 9a00h
+	dw 0000fh
+
+	; selector for transition into real mode	
+	dw 0ffffh 
+	dw 00000h
+	dw 09200h	; access = 0a2h = RW | PR
+	dw 0000fh	; flags = 0h = 16 bit, byte granularity 
+
+kernel_gdt_ptr: 
+	dw kernel_gdt_ptr - kernel_gdt - 1
+	dd kernel_gdt
+
+kernel_idt_ptr:	
+	dw 0
+	dw 0,0
+
+realmode_idt_ptr:
+	dw 400h
+	dw 0,0
 
 ;=======================	Real Mode int call wrapper	================
 
@@ -82,17 +126,12 @@ EndOutRegisters:
 
 OutRegistersAddress:
 		dd	0
-RealModeInterruptToCall:
-		dd	0
 BIOSCarryFlag:
 		dd	0
 ProtectedModeStackPointer:
 		dd	0
 ProtectedModeStackSegment:
 		dd	0
-RealModeReturnAddress:
-		dd	0
-
 
 	align	4
 
@@ -138,7 +177,7 @@ boot_realModeIntWrapper:
 
 	;	Store interrupt number 
 	mov	eax, [ebp + 08h]
-	mov	[RealModeInterruptToCall], eax
+	mov	[RealModeExecInt + 1], al
 
 	;	Preserve stack pointer and stack segment
 
@@ -148,10 +187,10 @@ boot_realModeIntWrapper:
 	mov	eax, esp
 	mov	[ProtectedModeStackPointer], eax
 
-	;	Set the return address from real mode
+	; 	Clear BIOS carry flag (stored DWORD)
 
-	mov	eax,RealModeReturn
-	mov	[RealModeReturnAddress], eax 
+	xor	eax,eax
+	mov	[BIOSCarryFlag], eax
 
 	; self-modify the address to jump to
 
@@ -167,21 +206,27 @@ boot_realModeIntWrapper:
 
 	; 2. Load 16 bit segment selectors
 
+	mov eax, 20h
+	mov ds, ax
+	mov fs, ax
+	mov gs, ax
+	mov es, ax	
+	mov ss ,ax
 
 	; 3. Switch to real mode
 
-	; 4. Set valid data segment
+	jmp 18h:RealModeSwitch
 
-
-	; 5. Load IVT	
-		
-	; 6. Jump to real mode code
-
-	jmp	RealModeReturn
+RealModeSwitch:		; we are now running in 16bit protected mode
+			; (selector 18h) 
 
 	BITS 16
 
-JumpToRealMode:
+	mov eax, cr0
+	and al, ~1
+	mov cr0, eax
+
+JumpToRealMode:		; we are now running in real mode
 	jmp	KERNEL_SEG:0a5a5h
 
 	nop
@@ -192,6 +237,11 @@ JumpToRealMode:
 
 RealModeReturn:
 
+	; reset data selector
+	mov ax, 10h
+	mov ds, ax
+	mov es, ax
+
 	; restore stack segment and stack pointer
 
 	mov	ax, [ProtectedModeStackSegment]
@@ -199,6 +249,16 @@ RealModeReturn:
 
 	mov	ss, ax
 	mov	esp, ebx 	
+
+	; Copy out registers to given structure
+
+	mov	eax, [OutRegistersAddress]
+	mov	edi, eax
+
+	mov	esi, OutRegisters
+	mov	ecx, (EndOutRegisters - OutRegisters) / 4
+
+	rep	movsd
 
 	; set return code
 
@@ -215,7 +275,10 @@ RealModeReturn:
 	pop 	ecx
 	pop    	ebx
   	pop    	ebp
-  	ret    
+  
+	; need to check here if we need to reenable interrupts
+
+	ret    
 
 ;====================== Real mode code for real mode wrapper	==============================
 
@@ -226,33 +289,125 @@ RealModeEnter:
 	; We are now in real mode
 	; set up data segment from code segment
 
+	mov ax, cs
+	mov ds, ax
+
+	; Load IDT
+
+	lidt [realmode_idt_ptr - kernel_entry_point]
 
 	; set up real mode stack
 
-
-	; ds:bp now points to the real mode wrapper data structure.
+	mov ax,0000h
+	mov ebx,7c00h
+	mov ss, ax
+	mov esp, ebx
 	
 	; set up registers
 
+	mov ax, [InES - kernel_entry_point]
+	mov es, ax
+
+	mov ax, [InFS - kernel_entry_point]
+	mov fs, ax
+
+	mov ax, [InGS - kernel_entry_point]
+	mov gs, ax
+
+	mov eax, [InESI - kernel_entry_point]
+	mov esi, eax
+
+	mov eax, [InEDI - kernel_entry_point]
+	mov edi, eax
+
+	mov eax, [InEBP - kernel_entry_point]
+	mov ebp, eax
+
+	mov ebx, [InEBX - kernel_entry_point]
+	mov ecx, [InECX - kernel_entry_point]
+	mov edx, [InEDX - kernel_entry_point] 
+
+	mov eax, [InEAX - kernel_entry_point] 
+
+	; execute interrupt
+
+RealModeExecInt:
+	int	55h
+
+	; store eax
+
+	mov [OutEAX - kernel_entry_point], eax
+
+	; store carry
+
+	jc Carry
+	
+	mov eax, 1
+	mov [BIOSCarryFlag - kernel_entry_point], eax
+
+Carry:
+	
+	; store rest of registers
+
+	mov [OutEBX - kernel_entry_point], ebx
+	mov [OutECX - kernel_entry_point], ecx
+	mov [OutEDX - kernel_entry_point], edx
+
+	mov eax, edi
+	mov [OutEDI - kernel_entry_point], eax
+
+	mov eax, esi
+	mov [OutESI - kernel_entry_point], eax
+
+	mov eax, ebp
+	mov [OutEBP - kernel_entry_point], eax
+
+	mov ax, fs
+	mov [OutFS - kernel_entry_point], ax
+
+	mov ax, gs
+	mov [OutGS - kernel_entry_point], ax
+
+	mov ax, es
+	mov [OutES - kernel_entry_point], ax
 
 	; Get ready to return to protected mode
-
-
-
-	; Load idt and gdt
 
 	; Make sure interrupts are clear
 
 	cli
 
+	; Load idt and gdt
+	
+	lidt [kernel_idt_ptr - kernel_entry_point]
+	lgdt [kernel_gdt_ptr - kernel_entry_point]
+
 	; Set protected mode bit 
 
+	mov eax, cr0
+	or  al,1
+	mov cr0, eax
+
+	BITS 32
 
 	; Clear prefetch queue
 
+	; Load cs
 
-	; Load return address
+	jmp ClearPrefetchQueue
+	nop
+	nop
+ClearPrefetchQueue:
 
+	; long jump to C program start
+	
+	db 066h		; for some reason, the disassembly is incorrect
+	jmp 08h:LoadCS
 
-	; Jump back to protected mode
+LoadCS:
+	
+	mov eax, RealModeReturn
+
+	jmp eax
+
 	align 4
